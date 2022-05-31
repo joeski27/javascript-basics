c29caf5e5a166eaecdefc781bf3832225ce622f1
Joseph Smith <jsmith2799@hotmail.com>
2022-05-31 18:52:00 +0100

    objects

--- src/objects.js
+++ src/objects.js
@@ -1,41 +1,53 @@
 const createPerson = (name, age) => {
-  // your code here
+  return {
+    name,
+    age
+  };
 };
 
 const getName = object => {
-  // your code here
+  return object.name;
 };
 
 const getProperty = (property, object) => {
-  // your code here
+  return object[property];
 };
 
 const hasProperty = (property, object) => {
-  // your code here
+  return object.hasOwnProperty(property);
 };
 
 const isOver65 = person => {
-  // your code here
+  return person.age > 65;
 };
 
 const getAges = people => {
-  // your code here
+  return people.map(person => person.age);
 };
 
 const findByName = (name, people) => {
-  // your code here
+  return people.find(person => person.name === name);
 };
 
 const findHondas = cars => {
-  // your code here
+  return cars.filter(object => object.manufacturer === 'Honda');
 };
 
 const averageAge = people => {
-  // your code here
+  const totalAge = people.reduce((acc, curr) => {
+    return acc + curr.age;
+  }, 0);
+  return totalAge / people.length;
 };
 
 const createTalkingPerson = (name, age) => {
-  // your code here
+  return {
+    name,
+    age,
+    introduce: person => {
+      return `Hi ${person}, my name is ${name} and I am ${age}!`;
+    }
+  };
 };
 
 module.exports = {
